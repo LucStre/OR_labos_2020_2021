dc51f18296b7fc2135ccdc02a18f042d4714bd12
Lucija <lucija.strejcek@fer.hr>
2021-12-13 14:08:13 +0100

    Add error handling

--- backend/routes/actor.js
+++ backend/routes/actor.js
@@ -3,11 +3,21 @@ var router = express.Router();
 const db = require("../db");
 
 router.get("/:id", async (req, res) => {
-  const data = await db.query(
-    "SELECT actor.*, array_to_string(array_agg(distinct award.awardname), ',') as awards, array_to_string(array_agg(distinct film.name), ',') as films FROM actor LEFT JOIN acts ON actor.actorid = acts.actorid LEFT JOIN film ON film.filmid = acts.filmid LEFT JOIN award ON award.actorid = actor.actorid WHERE actor.actorid = $1 GROUP BY actor.actorid",
-    [req.params.id]
-  );
-  res.json({ data: data.rows });
+  if (!Number.isInteger(Number(req.params.id))) {
+    res.status(400).send("Actor id must be integer!");
+  } else {
+    const data = await db.query(
+      "SELECT actor.*, array_to_string(array_agg(distinct award.awardname), ',') as awards, array_to_string(array_agg(distinct film.name), ',') as films FROM actor LEFT JOIN acts ON actor.actorid = acts.actorid LEFT JOIN film ON film.filmid = acts.filmid LEFT JOIN award ON award.actorid = actor.actorid WHERE actor.actorid = $1 GROUP BY actor.actorid",
+      [req.params.id]
+    );
+    if (!data.rowCount) {
+      res
+        .status(404)
+        .send("Actor with id " + req.params.id + " does not exist.");
+    } else {
+      res.json({ data: data.rows });
+    }
+  }
 });
 
 module.exports = router;
--- backend/routes/isacting.js
+++ backend/routes/isacting.js
@@ -3,11 +3,15 @@ var router = express.Router();
 const db = require("../db");
 
 router.get("/:year", async (req, res) => {
-  const data = await db.query(
-    "SELECT actor.*, array_to_string(array_agg(distinct award.awardname), ',') as awards, array_to_string(array_agg(distinct film.name), ',') as films FROM actor LEFT JOIN acts ON actor.actorid = acts.actorid LEFT JOIN film ON film.filmid = acts.filmid LEFT JOIN award ON award.actorid = actor.actorid WHERE actor.startyear <= $1 GROUP BY actor.actorid",
-    [req.params.year]
-  );
-  res.json({ data: data.rows });
+  if (!Number.isInteger(Number(req.params.year))) {
+    res.status(400).send("You must enter a year!");
+  } else {
+    const data = await db.query(
+      "SELECT actor.*, array_to_string(array_agg(distinct award.awardname), ',') as awards, array_to_string(array_agg(distinct film.name), ',') as films FROM actor LEFT JOIN acts ON actor.actorid = acts.actorid LEFT JOIN film ON film.filmid = acts.filmid LEFT JOIN award ON award.actorid = actor.actorid WHERE actor.startyear <= $1 GROUP BY actor.actorid",
+      [req.params.year]
+    );
+    res.json({ data: data.rows });
+  }
 });
 
 module.exports = router;
