9dacbb81fa8511924319d835b4bc3bc0bf94af8c
Lucija <lucija.strejcek@fer.hr>
2021-12-13 17:00:08 +0100

    Add post method

--- backend/routes/actors.js
+++ backend/routes/actors.js
@@ -9,4 +9,26 @@ router.get("/", async (req, res) => {
   res.json({ data: data.rows });
 });
 
+router.post("/", async (req, res) => {
+  await db.query(
+    "INSERT INTO actor (name, surname, middlename, birthdate, birthplace, height, marriage, children, siblings, startyear, favfood, favcolor, favbook) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
+    [
+      req.body.name,
+      req.body.surname,
+      req.body.middlename == undefined ? null : req.body.middlename,
+      req.body.birthdate,
+      req.body.birthplace,
+      req.body.height,
+      req.body.marriage,
+      req.body.children,
+      req.body.siblings,
+      req.body.startyear,
+      req.body.favfood == undefined ? null : req.body.favfood,
+      req.body.favcolor == undefined ? null : req.body.favcolor,
+      req.body.favbook == undefined ? null : req.body.favbook,
+    ]
+  );
+  res.status(201).send("New actor created.");
+});
+
 module.exports = router;
