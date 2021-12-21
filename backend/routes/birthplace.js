a8c894251cb3ffe3245e8949eaf5cb4fb303c42d
Lucija <lucija.strejcek@fer.hr>
2021-12-21 07:56:06 +0100

    Add error handling

--- backend/routes/birthplace.js
+++ backend/routes/birthplace.js
@@ -7,7 +7,13 @@ router.get("/:place", async (req, res) => {
     "SELECT actor.*, array_to_string(array_agg(distinct award.awardname), ',') as awards, array_to_string(array_agg(distinct film.name), ',') as films FROM actor LEFT JOIN acts ON actor.actorid = acts.actorid LEFT JOIN film ON film.filmid = acts.filmid LEFT JOIN award ON award.actorid = actor.actorid WHERE actor.birthplace = $1::text GROUP BY actor.actorid",
     [req.params.place]
   );
-  res.json({ data: data.rows });
+  if (data.rowCount == 0) {
+    res
+      .status(404)
+      .send("Actor with birthplace " + req.params.place + " does not exist.");
+  } else {
+    res.json({ data: data.rows });
+  }
 });
 
 module.exports = router;
